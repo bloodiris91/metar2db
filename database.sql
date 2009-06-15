cfc397749cf577b772faaab88781fee65a7dc0d0
timo.taskinen <timo.taskinen@9e9b3bc4-59a5-11de-bbaa-f1a44a2b9d47>
2009-06-15 14:00:44 +0000

    Very basic functionality. Not usable yet.

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `metar` (
+`id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`station` CHAR( 4 ) NOT NULL ,
+`timestamp` DATETIME NOT NULL ,
+`temperature` INT NOT NULL ,
+UNIQUE (
+`station` ,
+`timestamp`
+)
+) ENGINE = MYISAM
