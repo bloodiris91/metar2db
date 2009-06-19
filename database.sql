2e182cab6d83908da656aba8d75719c753087601
timo.taskinen <timo.taskinen@9e9b3bc4-59a5-11de-bbaa-f1a44a2b9d47>
2009-06-19 16:53:49 +0000

    Drop existing database table.

--- database.sql
+++ database.sql
@@ -1,10 +1,12 @@
+DROP TABLE IF EXISTS `metar`;
+
 CREATE TABLE `metar` (
-`id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-`station` CHAR( 4 ) NOT NULL ,
-`timestamp` DATETIME NOT NULL ,
-`temperature` INT NOT NULL ,
+  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  `station` CHAR( 4 ) NOT NULL ,
+  `timestamp` DATETIME NOT NULL ,
+  `temperature` INT NOT NULL ,
 UNIQUE (
-`station` ,
-`timestamp`
+  `station` ,
+  `timestamp`
 )
 ) ENGINE = MYISAM
